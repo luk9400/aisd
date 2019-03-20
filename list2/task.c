cfcd3427edfdf87d56b53181b9caf3f2414f4876
luk9400 <lukaszbratos@gmail.com>
2019-03-20 12:28:39 +0100

    task2

---
+++ list2/task.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int greater(int a, int b) {
+  return a > b;
+}
+
+int less(int a,int b) {
+  return a < b;
+}
+
+int select_sort(int* tab, int size, int (*compare)(int, int)) {
+  for (int i = 0; i < size - 1; i++) {
+    int min = i;
+    for (int j = i + 1; j < size; j++) {
+      if ((*compare)(tab[j], tab[min])) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      int tmp = tab[i];
+      tab[i] = tab[min];
+      tab[min] = tmp;
+    }
+  }
+}
+
+int insert_sort(int* tab, int size) {
+  for (int i = 0; i < size; i++) {
+
+  }
+}
+
+void print_tab(int* tab, int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d\n", tab[i]);
+  }
+}
+
+int main() {
+  int size = 5;
+  int* tab = malloc(size * sizeof(int));
+  tab[0] = 2;
+  tab[1] = 1;
+  tab[2] = 1;
+  tab[3] = 3;
+  tab[4] = 7;
+
+  // for (int i = 0; i < 1000; i++) {
+  //   tab[i] = i;
+  // }
+
+  select_sort(tab, size, &less);
+
+  print_tab(tab, size);
+
+  return 0;
+}
