5d47ad1f3bb816b0a97cce793d87f16b4977628c
luk9400 <lukaszbratos@gmail.com>
2019-04-05 22:51:05 +0200

    kopcowansko

--- list2/task.c
+++ list2/task.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+char type_flag = '0';
+int desc_flag = 0;
+int stat_flag = 0;
+
 
 int greater(int a, int b) {
   return a > b;
@@ -121,14 +128,117 @@ void print_tab(int* tab, int size) {
 }
 
 int main() {
-  int size = 5;
-  int* tab = malloc(size * sizeof(int));
-  tab[0] = 2;
-  tab[1] = 1;
-  tab[2] = 1;
-  tab[3] = 7;
-  tab[4] = 3;
+  // int size = 5;
+  // int* tab = malloc(size * sizeof(int));
+  // tab[0] = 2;
+  // tab[1] = 1;
+  // tab[2] = 1;
+  // tab[3] = 7;
+  // tab[4] = 3;
 
+  int c;
+  int option_index;
+  struct option long_options[] = {
+    {"asc", no_argument, 0, 'a'},
+    {"desc", no_argument, 0, 'd'},
+    {"type", required_argument, 0, 't'},
+    {"stat", required_argument, 0, 's'},
+    {0, 0, 0, 0}
+  };
+
+  while ((c = getopt_long(argc, argv, "ts", long_options, &option_index)) != -1) {
+    option_index = 0;
+
+    switch (c) {
+      case 'a': {
+        desc_flag = 0;
+        break;
+      }
+      case 'd': {
+        desc_flag = 1;
+        break;
+      }
+      case 's': {
+        // TODO
+        break;
+      }
+      case 't': {
+        if (strcmp(optarg, "select") == 0) {
+          type_flag = 's';
+        } else if (strcmp(optarg, "insert") == 0) {
+          type_flag = 'i';
+        } else if (strcmp(optarg, "quick") == 0) {
+          type_flag = 'q';
+        } else if (strcmp(optarg, "heap") == 0) {
+          type_flag = 'h';
+        } else if (strcmp(optarg, "mquick") == 0) {
+          type_flag = 'm';
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    if (stat_flag) {
+
+    } else {
+      int size;
+      printf("How many elements do you wish to sort?\n");
+      scanf("%d\n", &size);
+      int* tab = malloc(size * sizeof(int));
+      printf("What elements?\n");
+      for (int i = 0; i < size; i++) {
+        scanf("%d\n", tab[i]);
+      }
+
+      switch (type_flag) {
+        case 's': {
+          if (desc_flag) {
+            select_sort(tab, size, &less);
+          } else {
+            select_sort(tab, size, &greater);
+          }
+          break;
+        }
+        case 'i': {
+          if (desc_flag) {
+            insert_sort(tab, size, &less);
+          } else {
+            insert_sort(tab, size, &greater);
+          }
+          break;
+        }
+        case 's': {
+          if (desc_flag) {
+            select_sort(tab, size, &less);
+          } else {
+            select_sort(tab, size, &greater);
+          }
+          break;
+        }
+        case 's': {
+          if (desc_flag) {
+            select_sort(tab, size, &less);
+          } else {
+            select_sort(tab, size, &greater);
+          }
+          break;
+        }
+        case 's': {
+          if (desc_flag) {
+            select_sort(tab, size, &less);
+          } else {
+            select_sort(tab, size, &greater);
+          }
+          break;
+        }
+      }
+      print_tab(tab, size);
+    }
+
+  }
   // for (int i = 0; i < 1000; i++) {
   //   tab[i] = i;
   // }
@@ -136,9 +246,9 @@ int main() {
   //select_sort(tab, size, &less);
   //insert_sort(tab, size, &less);
   //quick_sort(tab, 0, size - 1, &less);
-  heap_sort(tab, size, &greater);
+  //heap_sort(tab, size, &greater);
 
-  print_tab(tab, size);
+  //print_tab(tab, size);
 
   return 0;
 }
